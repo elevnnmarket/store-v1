e4e70468271eeba183d01382b23e9d5226df8934
Matthew Shrider <shrid.dev@gmail.com>
2019-07-20 00:37:44 -0400

    chore(staging): added staging scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Deploy docker to staging
+echo "$(pwd) >>>>> Deploy"
+
+STAGING_DIR=${ELEVNN_STAGING:="~/staging"}
+echo "Staging directory: $STAGING_DIR"
+echo "Deleting Staging ($STAGING_DIR)"
+rm -rf $STAGING_DIR
+
+echo "Copying to staging ($STAGING_DIR)"
+rsync . $STAGING_DIR
+
+STAGING_DOCKER_PATH=$STAGING_DIR/wordpress/docker-compose.yml
+echo "Starting up docker at $STAGING_DOCKER_PATH"
+docker-compose -f $STAGING_DOCKER_PATH up
