1812cefae19b61bdb573bc1cd5c5b19e8fe35563
Matthew Shrider <shrid.dev@gmail.com>
2019-07-20 01:25:24 -0400

    chore: create staging directory if it did not exist, prevents build error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,7 @@ echo "Deleting Staging ($STAGING_DIR)"
 rm -rf $STAGING_DIR
 
 echo "Copying to staging ($STAGING_DIR)"
+mkdir -P $STAGING_DIR/
 cp -a ./. $STAGING_DIR/
 
 STAGING_DOCKER_PATH=$STAGING_DIR/wordpress/docker-compose.yml
