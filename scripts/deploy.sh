37624203c4e83e0dd7697724372873731d70d4f6
Matthew Shrider <shrid.dev@gmail.com>
2019-07-24 18:20:20 -0400

    fix(build): Take down docker before trying to delete deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,11 @@
 echo "$(pwd) >>>>> Deploy"
 
 STAGING_DIR=$DEPLOY_LOCATION
+STAGING_DOCKER_PATH=$STAGING_DIR/wordpress/docker-compose.yml
 echo "Staging directory: $STAGING_DIR"
+echo "Taking down Staging ($STAGING_DOCKER_PATH)"
+[ -f $STAGING_DOCKER_PATH ] && docker-compose -f $STAGING_DOCKER_PATH down
+
 echo "Deleting Staging ($STAGING_DIR)"
 rm -rf $STAGING_DIR
 
@@ -12,6 +16,5 @@ echo "Copying to staging ($STAGING_DIR)"
 mkdir -p $STAGING_DIR/
 cp -a . $STAGING_DIR
 
-STAGING_DOCKER_PATH=$STAGING_DIR/wordpress/docker-compose.yml
 echo "Starting up docker at $STAGING_DOCKER_PATH"
 docker-compose -f $STAGING_DOCKER_PATH up -d
