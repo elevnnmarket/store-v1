f8340b4a6520727984bcb408293cccc51a2aecee
Matthew Shrider <shrid.dev@gmail.com>
2019-07-26 19:06:43 -0400

    fix(deploy): Changed our deployments to use a secret file from CI
    This was because we dont want public-facing internet to be able to
    upload nefarious scripts and XSS attacks that can target our developers
    when they are running plugins from staging.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,12 @@
 # Deploy docker to staging
 echo "$(pwd) >>>>> Deploy"
 
+# File paths and secrets from CI server, do not put these in source control
+ENV_FILE=$ENV_LOCATION
 STAGING_DIR=$DEPLOY_LOCATION
 STAGING_DOCKER_PATH=$STAGING_DIR/wordpress/docker-compose.yml
+SECRET_FILE_DOCKER_LOCATION=$STAGING_DIR/wordpress/wordpress.env
+
 echo "Staging directory: $STAGING_DIR"
 echo "Taking down Staging ($STAGING_DOCKER_PATH)"
 [ -f $STAGING_DOCKER_PATH ] && docker-compose -f $STAGING_DOCKER_PATH down
@@ -15,6 +19,9 @@ rm -rf $STAGING_DIR
 echo "Copying to staging ($STAGING_DIR)"
 mkdir -p $STAGING_DIR/
 cp -a . $STAGING_DIR
+
+echo "Copying private files"
+cp $ENV_FILE $SECRET_FILE_DOCKER_LOCATION
 
 echo "Starting up docker at $STAGING_DOCKER_PATH"
 docker-compose -f $STAGING_DOCKER_PATH up -d
